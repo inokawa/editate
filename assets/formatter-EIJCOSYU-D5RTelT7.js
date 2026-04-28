d83dbcb320b0ad3617bffc88db150aa58ae3c9ce
inokawa <inokawa@users.noreply.github.com>
2026-04-28 09:49:53 +0000

    deploy: 8d9eae20dd617a0b0aa88ac6d55eee6bc3f1e081

---
+++ assets/formatter-EIJCOSYU-D5RTelT7.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{f as t,p as n}from"./iframe-Bk1_cRbb.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-B8zMAbhF.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
