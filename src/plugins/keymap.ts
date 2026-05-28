0ee15c63a449c8fe6e65ef0865ee3ecbe0ce47f4
inokawa <48897392+inokawa@users.noreply.github.com>
2026-05-28 10:28:45 +0900

    Refactor keymap plugin

--- src/plugins/keymap.ts
+++ src/plugins/keymap.ts
@@ -1,19 +1,13 @@
 import type { Editor } from "../editor.js";
-import {
-  keymap,
-  type KeyString,
-  type KeyboardHook,
-} from "../hooks/keyboard.js";
+import { keymap, type KeyString } from "../hooks/keyboard.js";
 import { keys } from "../utils.js";
 
 export function keymapPlugin<K extends KeyString>(
   editor: Editor,
-  bindings: Record<K, KeyboardHook>,
+  bindings: Record<K, () => void>,
 ) {
   keys(bindings).forEach((k) => {
-    editor.hook(
-      "keyboard",
-      keymap(k as KeyString, bindings[k as keyof typeof bindings]!),
-    );
+    const fn = keymap(k as KeyString, bindings[k as keyof typeof bindings]!);
+    editor.hook("keyboard", (e) => fn(e));
   });
 }
