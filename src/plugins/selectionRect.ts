cc7b1e07f9c2c70bcf9a66d361a34b45dde50f01
inokawa <48897392+inokawa@users.noreply.github.com>
2026-06-01 12:36:22 +0900

    Add selection rect plugin

---
+++ src/plugins/selectionRect.ts
@@ -0,0 +1,37 @@
+import { selectionToDomSelection } from "../doc/node.js";
+import { selectionToRange } from "../dom/index.js";
+import type { Editor } from "../editor.js";
+
+/**
+ * A plugin to get the bounding rect of selection on selection change
+ */
+export function selectionRectPlugin(
+  editor: Editor,
+  onSelectionChange: (getRect: () => DOMRectReadOnly) => void,
+) {
+  editor.hook("mount", (element, parser) => {
+    let mounted = true;
+    let queued = false;
+    const cleanup = editor.on("selectionchange", () => {
+      if (queued) return;
+      queued = true;
+      requestAnimationFrame(() => {
+        queued = false;
+        if (!mounted) return;
+        onSelectionChange(() => {
+          const selection = editor.selection;
+          return selectionToRange(
+            element,
+            parser,
+            selectionToDomSelection(editor.doc, selection),
+            selection[0] - selection[1],
+          ).getBoundingClientRect();
+        });
+      });
+    });
+    return () => {
+      mounted = false;
+      cleanup();
+    };
+  });
+}
