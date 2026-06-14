ea6f3557f2ad7dfa0e4702765868167e2df92971
inokawa <inokawa@users.noreply.github.com>
2026-06-14 11:30:16 +0000

    deploy: 18b5cd84e661ef6eb9785fc947f51e992ff9eae3

--- assets/react-gDONKVF2.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./DocsRenderer-LL677BLK-n4IvMaHu.js";t();export{e as MDXProvider};
--- dom/parser.d.ts
+++ dom/parser.d.ts
@@ -1,7 +1,6 @@
 interface ParserContext {
 }
 export interface Parser {
-    <T>(scopeFn: (ctx: ParserContext) => T, root: Node, startNode?: Node): T;
-    <T>(scopeFn: (ctx: ParserContext) => T): T;
+    <T>(scopeFn: (ctx: ParserContext) => T, root?: Node): T;
 }
 export {};
