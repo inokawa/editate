a5be2cec383fae95b704059f3ad7b4e203f13f92
inokawa <inokawa@users.noreply.github.com>
2026-06-17 12:17:25 +0000

    deploy: 7ff202be8225bcfdb324fc409f0f79a5826f4839

---
+++ assets/react-CX7b34I2.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-LL677BLK-DnG2HVNP.js";t();export{e as MDXProvider};
--- plugins/transfer/htmlTransfer.d.ts
+++ plugins/transfer/htmlTransfer.d.ts
@@ -1,9 +1,14 @@
 import { Editor } from '../../editor.js';
 import { DocNode, InferInlineNode, TextNode } from '../../doc/types.js';
+type HtmlSerializers<T extends DocNode> = Partial<{
+    [key in keyof HTMLElementTagNameMap]: (node: HTMLElementTagNameMap[key]) => Exclude<InferInlineNode<T>, TextNode> | void;
+}> & {
+    text: (t: string) => Extract<InferInlineNode<T>, TextNode>;
+};
 /**
  * A plugin to handle copying / pasting HTML
  */
 export declare function htmlTransferPlugin<T extends DocNode>(editor: Editor<T>, options: {
-    serializeText: (t: string) => Extract<InferInlineNode<T>, TextNode>;
-    serializers?: ((node: HTMLElement) => Exclude<InferInlineNode<T>, TextNode> | void)[];
+    serializers: HtmlSerializers<T>;
 }): void;
+export {};
