3150ad778779631dbdc3f87a8b86468bfd8e1cba
inokawa <48897392+inokawa@users.noreply.github.com>
2026-06-23 22:15:15 +0900

    Update node.spec.ts

--- src/doc/node.spec.ts
+++ src/doc/node.spec.ts
@@ -1,35 +1,76 @@
 import { describe, expect, it } from "vitest";
-import { sliceFragment, getNodeSize } from "./node.js";
+import { sliceFragment } from "./node.js";
 import { type DocNode, type Fragment, type Range } from "./types.js";
 
 describe(sliceFragment.name, () => {
+  const text0 = "abcde";
+  const text1 = "fghij";
+  const text2 = "klmno";
   const doc: DocNode = {
     children: [
-      { children: [{ text: "Hello" }] },
-      { children: [{ text: "world" }] },
+      { children: [{ text: text0 }] },
+      { children: [{ text: text1 }] },
+      { children: [{ text: text2 }] },
     ],
   };
-  const size0 = getNodeSize(doc.children[0]!);
-  const size1 = getNodeSize(doc.children[1]!);
   it.each<[Range, Fragment]>([
     [[1, 0], []],
+    [[1, 1], []],
     [[0, 0], []],
-    [[0, size0], [{ children: [{ text: "Hello" }] }]],
+    [[0, 1], [{ children: [{ text: text0.slice(0, 1) }] }]],
+    [[0, text0.length], [{ children: [{ text: text0 }] }]],
     [
-      [0, size0 + 1],
-      [{ children: [{ text: "Hello" }] }, { children: [{ text: "" }] }],
+      [0, text0.length + 1],
+      [{ children: [{ text: text0 }] }, { children: [{ text: "" }] }],
     ],
     [
-      [0, size0 + 2],
-      [{ children: [{ text: "Hello" }] }, { children: [{ text: "w" }] }],
+      [0, text0.length + 2],
+      [
+        { children: [{ text: text0 }] },
+        { children: [{ text: text1.slice(0, 1) }] },
+      ],
     ],
     [
-      [size0, size0 + 1],
+      [0, text0.length + 1 + text1.length],
+      [{ children: [{ text: text0 }] }, { children: [{ text: text1 }] }],
+    ],
+    [
+      [0, text0.length + 1 + text1.length + 1],
+      [
+        { children: [{ text: text0 }] },
+        { children: [{ text: text1 }] },
+        { children: [{ text: "" }] },
+      ],
+    ],
+    [
+      [0, text0.length + 1 + text1.length + 2],
+      [
+        { children: [{ text: text0 }] },
+        { children: [{ text: text1 }] },
+        { children: [{ text: text2.slice(0, 1) }] },
+      ],
+    ],
+    [[0, text0.length + 1 + text1.length + 1 + text2.length], doc.children],
+    [[0, Infinity], doc.children],
+    [
+      [3, text0.length + 2],
+      [
+        { children: [{ text: text0.slice(3) }] },
+        { children: [{ text: text1.slice(0, 1) }] },
+      ],
+    ],
+    [
+      [3, text0.length + 1 + text1.length + 2],
+      [
+        { children: [{ text: text0.slice(3) }] },
+        { children: [{ text: text1 }] },
+        { children: [{ text: text2.slice(0, 1) }] },
+      ],
+    ],
+    [
+      [text0.length, text0.length + 1],
       [{ children: [{ text: "" }] }, { children: [{ text: "" }] }],
     ],
-    [[size0 + 1, size0 + size1], [{ children: [{ text: "worl" }] }]],
-    [[0, size0 + size1 + 1], doc.children],
-    [[0, Infinity], doc.children],
   ])(`$0`, (range, res) => {
     expect(sliceFragment(doc, ...range)).toEqual(res);
   });
