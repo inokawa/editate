42cc1183384d0512ca00c33b1cc8c5e2821e2262
inokawa <inokawa@users.noreply.github.com>
2026-07-04 08:48:06 +0000

    deploy: 63982ed65b83714611d39d01274119309d6cdba7

--- doc/types-infer.d.ts
+++ doc/types-infer.d.ts
@@ -1,11 +1,10 @@
 import { DocNode, Node, TextNode } from './types.js';
-type Flatten<T> = T extends any ? T : never;
-export type InferBlockNode<T extends Node> = Flatten<T extends {
+export type InferBlockNode<T extends Node> = T extends {
     children: readonly (infer N extends Node)[];
-} ? T | InferBlockNode<N> : never>;
-export type InferInlineNode<T extends Node> = Flatten<T extends {
+} ? T | InferBlockNode<N> : never;
+export type InferInlineNode<T extends Node> = T extends {
     children: readonly (infer N extends Node)[];
-} ? InferInlineNode<N> : T>;
+} ? InferInlineNode<N> : T;
 export type InferVoidNode<T extends DocNode> = Exclude<InferInlineNode<T>, TextNode>;
 export type InferTextNode<T extends DocNode> = Extract<InferInlineNode<T>, TextNode>;
 type FlattenAllNodes<T extends Node> = T extends any ? T | (T extends {
