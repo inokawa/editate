28039d81cd24ff6886027fa1d55e6877db320dc3
inokawa <inokawa@users.noreply.github.com>
2026-07-13 11:35:18 +0000

    deploy: f100f2abe6c8669488333fdff10297d6b456a8d7

--- dom/index.d.ts
+++ dom/index.d.ts
@@ -1,5 +1,5 @@
 import { Parser } from './parser.js';
-import { DomSelection } from '../doc/types.js';
+import { DocNode, Selection as JsSelection } from '../doc/types.js';
 export { createParser, TOKEN_TEXT, TOKEN_VOID, TOKEN_SOFT_BREAK, TOKEN_BLOCK, } from './parser.js';
 export { defaultIsBlockNode } from './default.js';
-export declare const selectionToRange: (root: Element, parse: Parser, [anchor, focus]: DomSelection, posDiff: number) => Range;
+export declare const selectionToRange: (root: Element, parse: Parser, doc: DocNode, sel: JsSelection) => Range;
