2d80fa4087d0a2b19d42c810dc692dd7ff1b7570
inokawa <inokawa@users.noreply.github.com>
2026-07-16 15:02:53 +0000

    deploy: d659c4f68a9dc6250263b3504171cdfb02c3ecf1

--- queries.d.ts
+++ queries.d.ts
@@ -2,6 +2,6 @@ import { InferInlineNode } from './doc/types-infer.js';
 import { DocNode, Range } from './doc/types.js';
 import { Editor } from './editor.js';
 /**
- * TODO
+ * Get leaf nodes that intersect with the selection or specified range.
  */
 export declare function LeafsInRange<T extends DocNode>(editor: Editor<T>, range?: Range): Generator<InferInlineNode<T>, void, void>;
