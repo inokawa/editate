634dd171396992d65c89c0aacc9e653e98691642
inokawa <48897392+inokawa@users.noreply.github.com>
2026-07-19 13:32:07 +0900

    Update history.spec.ts

--- src/plugins/history.spec.ts
+++ src/plugins/history.spec.ts
@@ -2,6 +2,7 @@ import { expect, it } from "vitest";
 import type { DocNode, Selection } from "../doc/types.js";
 import { createEditor } from "../editor.js";
 import { Redo, Undo } from "./history.js";
+import { getNodeSize } from "../doc/node.js";
 
 it("undo insert text", () => {
   const doc: DocNode = {
@@ -267,6 +268,129 @@ it("undo replace linebreak", () => {
   const updatedSelection: Selection = [
     selection[0] + text.length,
     selection[0] + text.length,
+  ];
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+
+  editor.exec(Undo);
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  editor.exec(Redo);
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+});
+
+it("undo insert text node", () => {
+  const doc: DocNode = {
+    children: [
+      { children: [{ text: "abcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const selection: Selection = [1, 1];
+  const editor = createEditor({ doc });
+  editor.selection = selection;
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  const text = "xyz";
+  editor.apply({
+    type: "insert_node",
+    at: selection[0],
+    fragment: [{ children: [{ text }] }],
+  });
+  const updatedDoc: DocNode = {
+    children: [
+      { children: [{ text: "axyzbcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const updatedSelection: Selection = [
+    selection[0] + text.length,
+    selection[1] + text.length,
+  ];
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+
+  editor.exec(Undo);
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  editor.exec(Redo);
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+});
+
+it("undo insert void node", () => {
+  const doc: DocNode = {
+    children: [
+      { children: [{ text: "abcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const selection: Selection = [1, 1];
+  const editor = createEditor({ doc });
+  editor.selection = selection;
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  editor.apply({
+    type: "insert_node",
+    at: selection[0],
+    fragment: [{ children: [{ foo: "bar" }] }],
+  });
+  const updatedDoc: DocNode = {
+    children: [
+      { children: [{ text: "a" }, { foo: "bar" }, { text: "bcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const updatedSelection: Selection = [selection[0] + 1, selection[1] + 1];
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+
+  editor.exec(Undo);
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  editor.exec(Redo);
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+});
+
+it("undo insert nodes", () => {
+  const doc: DocNode = {
+    children: [
+      { children: [{ text: "abcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const selection: Selection = [1, 1];
+  const editor = createEditor({ doc });
+  editor.selection = selection;
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  const fragment = [
+    { children: [{ text: "ABC" }] },
+    { children: [{ text: "DEFG" }] },
+  ];
+  editor.apply({
+    type: "insert_node",
+    at: selection[0],
+    fragment: fragment,
+  });
+  const updatedDoc: DocNode = {
+    children: [
+      { children: [{ text: "aABC" }] },
+      { children: [{ text: "DEFGbcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const updatedSelection: Selection = [
+    selection[0] + getNodeSize({ children: fragment }),
+    selection[1] + getNodeSize({ children: fragment }),
   ];
   expect(editor.doc).toEqual(updatedDoc);
   expect(editor.selection).toEqual(updatedSelection);
