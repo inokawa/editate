7d1509d24d0417922072e9f716b304968eee86ce
inokawa <inokawa@users.noreply.github.com>
2026-07-20 04:51:58 +0000

    deploy: 604fe58ad1c5be7a55eedec3732ec1d9b3d82324

--- doc/node.d.ts
+++ doc/node.d.ts
@@ -1,10 +1,10 @@
 import { InferInlineNode, InferVoidNode } from './types-infer.js';
-import { BlockNode, DocNode, InlineNode, Node, Path, DomPosition } from './types.js';
+import { BlockNode, DocNode, InlineNode, Node, Path, DomPosition, Range } from './types.js';
 export declare const getNodeSize: (node: Node) => number;
 export declare const getChildAt: <T extends BlockNode>({ children }: T, offset: number) => [node: T["children"][number], offset: number, index: number] | null;
 export declare const getBlockAt: (node: DocNode | BlockNode, offset: number) => [node: BlockNode, offset: number, path: Path];
 export declare const getLeafAt: (node: DocNode | BlockNode, offset: number) => [node: InlineNode, offset: number] | null;
 export declare const offsetToPosition: (node: DocNode | BlockNode, offset: number) => DomPosition;
-export declare function iterChilds<T extends Node>(node: T, start: number, end: number): Generator<[node: Node, offset: number], void, void>;
-export declare function iterLeafs<T extends Node>(node: T, start: number, end: number): Generator<[node: InferInlineNode<T>, offset: number], void, void>;
+export declare function iterNodes<T extends Node>(node: T, [start, end]: Range): Generator<[node: Node, offset: number], void, void>;
+export declare function iterLeafs<T extends Node>(node: T, [start, end]: Range): Generator<[node: InferInlineNode<T>, offset: number], void, void>;
 export declare const sliceText: <T extends Node>(node: T, start?: number, end?: number, voidToString?: (node: InferVoidNode<T>) => string) => string;
